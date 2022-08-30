15568cb9f922380df38862f20ccf21d7d6cf4c48
Ted Diño <ted23@tutanota.com>
2022-08-30 16:39:18 +0800

    Styling: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightPrimary: "#333333",
+        darkPrimary: "#E0E0E0",
+        secondary: "#828282",
+        accent: "#2f80ed",
+        borderClr: "#BDBDBD",
+      },
+    },
   },
   plugins: [],
-}
+};
