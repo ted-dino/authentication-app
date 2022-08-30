f0b4c8480ad92404a192f8462db26eab8cf0f95e
Ted Diño <ted23@tutanota.com>
2022-08-30 16:39:36 +0800

    Styling: add custom font

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="origin"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
