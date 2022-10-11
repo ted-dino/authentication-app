0c7b9e51d56cacca6355534239d4c46e819e783e
Ted Diño <ted23@tutanota.com>
2022-10-11 22:37:58 +0800

    refactor: extend user interface

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,21 @@
-import NextAuth, { DefaultSession } from "next-auth"
-import { JWT } from "next-auth/jwt"
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth/jwt" {
-    interface JWT {
-      bio?: string,
-      password?:string
-    }
+  interface JWT {
+    bio?: string;
+    password?: string | null;
   }
+}
 
 declare module "next-auth" {
   interface Session {
     user: {
-      bio?: string,
-      password?: string
-    } & DefaultSession["user"]
+      bio?: string | null;
+      password?: string | null;
+    } & DefaultSession["user"];
+  }
+  interface User {
+    password: string | null;
   }
 }
