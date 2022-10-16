dc7ad9eb66ca9fcb528052553ca4409e93872aad
Ted Diño <ted23@tutanota.com>
2022-10-16 15:55:25 +0800

    feat: protect route

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from "next";
+import { unstable_getServerSession } from "next-auth";
 import { signIn } from "next-auth/react";
 import { useTheme } from "next-themes";
 import Head from "next/head";
@@ -6,6 +8,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
 import Password from "../components/Password";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 const Login = () => {
   const { theme } = useTheme();
@@ -147,6 +150,27 @@ const Login = () => {
       </div>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 };
 
 export default Login;
