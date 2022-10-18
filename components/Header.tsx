1e24afa3c68159d90af6a44e0da3ee611319d772
Ted Diño <ted23@tutanota.com>
2022-10-18 15:38:39 +0800

    feat: show profile settings when logged in

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
+import { signOut, useSession } from "next-auth/react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const { data: session } = useSession();
 
   useEffect(() => setMounted(true), []);
 
@@ -54,22 +56,19 @@ const Header = () => {
       );
     }
   };
+
   return (
     <header className="flex items-center justify-between">
       <div className="logo grid place-content-center">
-        <Link href="/">
-          <a>
-            <Image
-              src={
-                theme === "dark"
-                  ? "/devchallenges-light.svg"
-                  : "/devchallenges-dark.svg"
-              }
-              width={120}
-              height={50}
-            />
-          </a>
-        </Link>
+        <Image
+          src={
+            theme === "dark"
+              ? "/devchallenges-light.svg"
+              : "/devchallenges-dark.svg"
+          }
+          width={120}
+          height={50}
+        />
       </div>
       <button
         className="block md:hidden"
@@ -109,10 +108,89 @@ const Header = () => {
           )}
         </span>
       </button>
-      {/* hidden */}
       <nav className="hidden md:block w-full ">
-        <ul className="h-full flex items-center justify-end">
-          <li>{themeSwitcher()}</li>
+        <ul className="h-full flex gap-2 items-center justify-end">
+          <li className="flex items-center">{themeSwitcher()}</li>
+          {session && session.user && (
+            <>
+              <li
+                onClick={() => setShowDropdown((prevState) => !prevState)}
+                className="relative flex items-center gap-2 cursor-pointer"
+              >
+                <img
+                  src={session.user.image as string}
+                  width={32}
+                  height={36}
+                  className="rounded-md"
+                />
+                <span>{session.user.name}</span>
+                {showDropdown && (
+                  <ul
+                    className={`${
+                      showDropdown ? "block" : "hidden"
+                    } grid grid-cols-1 gap-2 absolute right-0 top-[40px] z-10 h-[174px] w-[188px] rounded-2xl pt-4 pl-5 pr-3 pb-3 border border-borderClr`}
+                  >
+                    <li className="p-2 flex items-center gap-2 hover:bg-[#f2f2f2] dark:hover:bg-[#c83929] rounded-md">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      <span>My Profile</span>
+                    </li>
+                    <li className="p-2 flex items-center gap-2 hover:bg-[#f2f2f2] dark:hover:bg-[#c83929] rounded-md">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M8.25 6.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM15.75 9.75a3 3 0 116 0 3 3 0 01-6 0zM2.25 9.75a3 3 0 116 0 3 3 0 01-6 0zM6.31 15.117A6.745 6.745 0 0112 12a6.745 6.745 0 016.709 7.498.75.75 0 01-.372.568A12.696 12.696 0 0112 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 01-.372-.568 6.787 6.787 0 011.019-4.38z"
+                          clipRule="evenodd"
+                        />
+                        <path d="M5.082 14.254a8.287 8.287 0 00-1.308 5.135 9.687 9.687 0 01-1.764-.44l-.115-.04a.563.563 0 01-.373-.487l-.01-.121a3.75 3.75 0 013.57-4.047zM20.226 19.389a8.287 8.287 0 00-1.308-5.135 3.75 3.75 0 013.57 4.047l-.01.121a.563.563 0 01-.373.486l-.115.04c-.567.2-1.156.349-1.764.441z" />
+                      </svg>
+                      <span>Group Chat</span>
+                    </li>
+                    <hr />
+                    <li className="p-2 flex items-center gap-2 hover:bg-[#f2f2f2] dark:hover:bg-[#c83929] rounded-md">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+
+                      <button
+                        onClick={() =>
+                          signOut({
+                            callbackUrl: "/login",
+                          })
+                        }
+                      >
+                        Sign out
+                      </button>
+                    </li>
+                  </ul>
+                )}
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
